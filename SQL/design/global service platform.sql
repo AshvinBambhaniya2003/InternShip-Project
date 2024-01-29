f0c6029c634390e8587e4b2087429901d8724d51
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-01-29 20:14:58 +0530

    fix: communication, demos and assigmentmaterial

--- SQL/design/global service platform.sql
+++ SQL/design/global service platform.sql
@@ -7,20 +7,9 @@
 Table users {
   user_id integer [primary key]
   username varchar
-  role varchar 
+  role BOOLEAN 
   name varchar
 }
-
-Table client {
-  client_id integer [primary key]
-  username varchar
-}
-
-Table creators {
-  creator_id integer [primary key]
-  username varchar
-}
-
 
 
 Table Skills {
@@ -32,6 +21,7 @@ Table Skills {
 Table Demos {
   demo_id integer [primary key]
   creator_id integer
+  skill_id integer
   demo_type varchar
   demo_path varchar
 }
@@ -64,52 +54,51 @@ TABLE Assignments {
   assignment_id integer [primary key]
   project_id integer 
   creator_id integer 
-  status varchar
+  status BOOLEAN
+  deadline date
 }
 
+Table AssignmentMaterials {
+  assigment_material_id integer [primary key]
+  assignment_id integer 
+  material_type varchar
+  path varchar
+}
 TABLE Communications {
   communication_id INT [PRIMARY KEY]
   assignment_id INT
+  sender_id INT
+  receiver_id INT
   message TEXT
-}
-
-TABLE Feedback {
-  feedback_id INT [PRIMARY KEY]
-  assignment_id INT
-  client_feedback TEXT
-}
-
-TABLE Deadlines {
-  deadline_id INT [PRIMARY KEY]
-  assignment_id INT
+  sent_at TIMESTAMP
+  is_read BOOLEAN 
 }
 
 
+Ref: "users"."user_id" < "creator_Skills"."creator_id"
 
-Ref: "users"."user_id" < "client"."client_id"
+Ref: "users"."user_id" < "Projects"."client_id"
 
-Ref: "users"."user_id" < "creators"."creator_id"
+Ref: "Projects"."project_id" < "ProjectMaterials"."project_id"
 
-Ref: "client"."client_id" < "Projects"."client_id"
-
-Ref: "creators"."creator_id" < "Demos"."creator_id"
-
-Ref: "Projects"."project_id" < "Assignments"."project_id"
-
-Ref: "creators"."creator_id" < "Assignments"."creator_id"
-
-Ref: "Categories"."category_name" < "Skills"."skill"
-
-Ref: "creators"."creator_id" < "Skills"."creator_id"
-
-Ref: "creators"."creator_id" < "creator_Skills"."creator_id"
-
-Ref: "Skills"."skill_id" < "creator_Skills"."skill_id"
+Ref: "users"."user_id" < "Assignments"."creator_id"
 
 Ref: "Assignments"."assignment_id" < "Communications"."assignment_id"
 
-Ref: "Assignments"."assignment_id" < "Feedback"."assignment_id"
+Ref: "Categories"."category_name" < "Skills"."skill"
 
-Ref: "Assignments"."assignment_id" < "Deadlines"."assignment_id"
+Ref: "Skills"."skill_id" < "Demos"."skill_id"
 
-Ref: "Projects"."project_id" < "ProjectMaterials"."project_id"
+Ref: "Skills"."skill_id" < "creator_Skills"."skill_id"
+
+Ref: "users"."user_id" < "Skills"."creator_id"
+
+Ref: "users"."user_id" < "Demos"."creator_id"
+
+Ref: "users"."user_id" < "Communications"."sender_id"
+
+Ref: "users"."user_id" < "Communications"."receiver_id"
+
+Ref: "Assignments"."assignment_id" < "AssignmentMaterials"."assignment_id"
+
+Ref: "Projects"."project_id" < "Assignments"."project_id"
