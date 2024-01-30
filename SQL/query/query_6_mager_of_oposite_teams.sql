369efa64d11edd4d4ec60cd06f72d2d6bc335a1a
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-01-30 14:06:41 +0530

    fix: fix query 6

--- SQL/query/query_6_mager_of_oposite_teams.sql
+++ SQL/query/query_6_mager_of_oposite_teams.sql
@@ -1,14 +1,15 @@
 -- Query-6 : Which managers have been manager of opponent teams? (Rephrased: Identify managers who have transitioned to become the manager of teams that were once opponents of the team they initially managed.)
 
-SELECT DISTINCT
-    m1.own_manager_name,
-    m1.club_id,
-    m1.opponent_id,
-    m1.game_id AS original_game_id,
-    m2.game_id AS new_game_id
+select DISTINCT
+    t1.own_manager_name,
+    t1.club_id,
+    t1.opponent_id,
+    t1.game_id AS original_game_id,
+    t2.game_id AS new_game_id
 FROM
-    clubs_games m1
+    clubs_games t1
 JOIN
-    clubs_games m2 ON m1.opponent_manager_name = m2.own_manager_name
-WHERE
-    m1.opponent_id = m2.club_id;
+    clubs_games t2 ON t1.own_manager_name IS NOT NULL
+                      AND t2.opponent_manager_name IS NOT NULL
+                      AND t1.opponent_id = t2.club_id 
+where t1.own_manager_name = t2.own_manager_name;
