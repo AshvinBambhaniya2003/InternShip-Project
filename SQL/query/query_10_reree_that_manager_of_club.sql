29ec63717c06cc114654baad0f49c6706be03d51
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-01-30 15:30:38 +0530

    fix: solve issue of query 10

--- SQL/query/query_10_reree_that_manager_of_club.sql
+++ SQL/query/query_10_reree_that_manager_of_club.sql
@@ -1,12 +1,30 @@
 -- Query-10 : Give list of referees who have been once a manager of a team.
 
-SELECT DISTINCT
-    g1.game_id,
-    g1.referee,
-    g1.home_club_id,
-    g1.home_club_manager_name,
-    g1.away_club_id,
-    g1.away_club_manager_name
-FROM games g1
-JOIN games g2 ON g1.referee = g2.home_club_manager_name OR g1.referee = g2.away_club_manager_name
-WHERE g1.referee IS NOT null;
+SELECT distinct 
+		*
+FROM  
+    (
+    SELECT
+        g1.game_id,
+        g1.referee,
+        g2.home_club_id,
+        g2.home_club_manager_name,
+        g2.away_club_id,
+        g2.away_club_manager_name
+    FROM 
+        games g1
+    JOIN games g2 ON g1.referee = g2.home_club_manager_name 
+    
+    UNION ALL
+
+    SELECT
+       	g1.game_id,
+        g1.referee,
+        g2.home_club_id,
+        g2.home_club_manager_name,
+        g2.away_club_id,
+        g2.away_club_manager_name
+    FROM 
+        games g1
+    JOIN games g2 ON g1.referee = g2.away_club_manager_name
+    )as t1;
