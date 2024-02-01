034948ddf01ef6a8cdd1a4e149b19cd630ae94fc
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-02-01 19:34:09 +0530

    fix: solve issue related forien key in demo and reassign project

--- SQL/design/global service platform.sql
+++ SQL/design/global service platform.sql
@@ -7,13 +7,15 @@
 Table users {
   user_id integer [primary key]
   username varchar [unique, not null]
-  role BOOLEAN 
+  role ENUM('client', 'creator') [not null]
   name varchar [not null]
 }
 
+
 Table Skills {
   skill_id integer [primary key]
-  skill varchar
+  skill varchar [not null]
+  description VARCHAR
 }
 
 Table Demos {
@@ -33,15 +35,18 @@ TABLE creator_Skills {
     creator_id integer [not null]
     skill_id integer [not null]
     Indexes {
-    (creator_id, skill_id) [pk]
+    (creator_id, skill_id) [pk] 
   }
 }
+
+Ref: creator_Skills.(creator_id, skill_id) - Demos.(creator_id, skill_id)
 
 Table Projects {
   project_id integer [primary key]
   client_id integer [not null]
   project_name varchar [not null]
   project_description varchar [not null]
+  project_status ENUM('open', 'locked') [not null] 
 }
 
 Table ProjectMaterials {
@@ -55,9 +60,13 @@ TABLE Assignments {
   assignment_id integer [primary key]
   project_id integer [not null]
   creator_id integer [not null]
-  status BOOLEAN
+  assignee_id INT
+  is_locked BOOLEAN [not null]
   deadline date
+  submission_path VARCHAR 
+  is_done BOOLEAN [not null]
 }
+
 
 Table AssignmentMaterials {
   assigment_material_id integer [primary key]
@@ -75,12 +84,14 @@ Table CommunicationMessages {
   sent_at TIMESTAMP
   is_read BOOLEAN 
 }
+
+
 Table Communications {
   communication_id INT [PRIMARY KEY]
   assignment_id INT [NOT NULL]
 }
 
-Ref: "CommunicationMessages"."communication_id" < "Communications"."communication_id"
+Ref: "CommunicationMessages"."communication_id" - "Communications"."communication_id"
 
 Ref: "users"."user_id" < "creator_Skills"."creator_id"
 
@@ -94,12 +105,8 @@ Ref: "Assignments"."assignment_id" < "AssignmentMaterials"."assignment_id"
 
 Ref: "Projects"."project_id" < "Assignments"."project_id"
 
-Ref: "creator_Skills"."creator_id" < "Demos"."creator_id"
-
-Ref: "creator_Skills"."skill_id" < "Demos"."skill_id"
-
 Ref: "Skills"."skill_id" < "creator_Skills"."skill_id"
 
-Ref: "creator_Skills"."creator_id" < "Assignments"."creator_id"
-
 Ref: "Assignments"."assignment_id" < "Communications"."assignment_id"
+
+Ref: "users"."user_id" < "Assignments"."creator_id"
