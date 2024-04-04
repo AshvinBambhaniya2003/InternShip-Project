36e00b015de375f3c9adba538f6ab0bde9224481
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-04-04 07:09:34 +0000

    feature: Navbar (#50)
    Add navbar using bootstrap and use cdn for this
    
    Reviewed-on: https://git.pride.improwised.dev/Onboarding-2024/Ashvin-Bambhaniya/pulls/50
    Reviewed-by: payal <payal@improwised.com>
    Co-authored-by: AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
    Co-committed-by: AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>

--- VueExercise/src/App.vue
+++ VueExercise/src/App.vue
@@ -1,6 +1,30 @@
-<script setup>
-</script>
-
 <template>
-  Hello World
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Location Identifier App</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" @click="activeTab = 'home'" href="#">Home</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container mt-5">
+    <div v-if="activeTab === 'home'">
+      Home
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const activeTab = ref('home');
+
+</script>
